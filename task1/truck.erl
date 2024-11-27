5fde418e24321d012dc2bd82af9259dfddf9958c
Jaime Russo <itsjaimerusso@gmail.com>
2024-11-27 12:00:33 +0000

    project cleanup and report rewrite

--- task1/truck.erl
+++ task1/truck.erl
@@ -1,19 +1,60 @@
 -module(truck).
--export([start/0]).
 
-start() -> process_packages(0).
+-export([start/4, loop/7]).
 
-%Need to make sure that only receives the package if the truck is not full (somehow with pattern maybe?)
-process_packages(Count) ->
+%% Starting the truck
+start(TruckId, MainPid, NumTrucks, BeltPid) ->
+    MaxCapacity = 20,  %% Default MaxCapacity
+    CurrentLoad = 0,  %% Initial load of 0
+    BeltEmpty = false, %% Initial state of the belt
+    io:format("Truck ~p: Starting with max capacity ~p~n", [TruckId, MaxCapacity]),
+    BeltPid ! {truck_dispatching, false},
+    spawn(?MODULE,
+          loop,
+          [TruckId, MainPid, NumTrucks, BeltPid, CurrentLoad, MaxCapacity, BeltEmpty]).
+
+%% Main loop of the truck
+loop(TruckId, MainPid, NumTrucks, BeltPid, CurrentLoad, MaxCapacity, BeltEmpty) ->
+    io:format("Truck ~p: Requesting Package~n", [TruckId]),
+    BeltPid ! {request_package, self()},
     receive
-        {PackageId, Conveyor} ->
-            NewCount = Count + 1,
-            io:format("Truck ~p: received Package ~p from Conveyor Belt ~p~n", [self(), PackageId, Conveyor]),
-            if
-                NewCount < 10 -> % truck capacity is 10, when it receives the 10th package it resets because this expression gets false
-                    process_packages(NewCount);
-                true ->
-                    io:format("Truck ~p: full, reseting...~n", [self()]),
-                    process_packages(0)
+        %% Empty belt signal received
+        empty_belt ->
+            io:format("Truck ~p: Received empty belt signal. Dispatching load~n", [TruckId]),
+            io:format("Truck ~p: Stopping~n", [TruckId]),
+            dispatch(TruckId, CurrentLoad, MainPid);
+        %% No packages available, retry after a short delay
+        no_package ->
+            io:format("Truck ~p: No packages available. Retrying~n", [TruckId]),
+            timer:sleep(500), %% Simulate waiting before retry
+            loop(TruckId, MainPid, NumTrucks, BeltPid, CurrentLoad, MaxCapacity, BeltEmpty);
+        %% Package received
+        {package, {PackageId, PackageSize}} ->
+            if PackageSize + CurrentLoad > MaxCapacity ->
+                   io:format("Truck ~p: Max capacity reached. Dispatching load~n", [TruckId]),
+                   dispatch(TruckId, CurrentLoad, MainPid, NumTrucks, BeltPid);
+               true ->
+                   io:format("Truck ~p: Loaded package ~p (size ~p)~n",
+                             [TruckId, PackageId, PackageSize]),
+                   timer:sleep(1000), %% Simulate loading delay
+                   loop(TruckId,
+                        MainPid,
+                        NumTrucks,
+                        BeltPid,
+                        CurrentLoad + PackageSize,
+                        MaxCapacity,
+                        BeltEmpty)
             end
     end.
+
+%% Dispatch logic
+dispatch(TruckId, CurrentLoad, MainPid) ->
+    io:format("Truck ~p: Final dispatching load ~p~n", [TruckId, CurrentLoad]),
+    io:format("Truck ~p: Stopped~n", [TruckId]),
+    MainPid ! {truck_stopped, TruckId}. %% Notify main process.
+
+dispatch(TruckId, CurrentLoad, MainPid, NumTrucks, BeltPid) ->
+    BeltPid ! {truck_dispatching, true},
+    io:format("Truck ~p: Dispatching load ~p~n", [TruckId, CurrentLoad]),
+    NewTruckId = TruckId + NumTrucks,
+    spawn(?MODULE, start, [NewTruckId, MainPid, NumTrucks, BeltPid]). %% Pass MainPid
